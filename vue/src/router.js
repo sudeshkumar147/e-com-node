f9d1fcea46dc763242fb8f217aa54c003c0b4b02
saksham <sudeshkumar147@gmail.com>
2019-04-13 00:15:29 +0530

    adding template

--- vue/src/router.js
+++ vue/src/router.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import Signup from './views/Signup.vue'
-import Login from './views/Login.vue'
+import Home from './components/Home.vue'
+import Signup from './components/Signup.vue'
+import Login from './components/Login.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -19,7 +19,7 @@ export default new Router({
             // route level code-splitting
             // this generates a separate chunk (about.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
-            component: () => import( /* webpackChunkName: "about" */ './views/About.vue')
+            component: () => import( /* webpackChunkName: "about" */ './components/About.vue')
         },
         {
             path: '/signup',
